dc91ba915c3b3fcf7393db6c25b0c066c0632faa
popas90 <bogdititupopa@gmail.com>
2016-09-01 21:19:55 +0300

    modify generate.sh

--- generate.sh
+++ generate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 printf 'Creating folder tree...'
-rm -r antlr_generated
+if [ -d "antlr_generated" ]; then
+  rm -rdf antlr_generated/
+fi
 mkdir antlr_generated
 mkdir antlr_generated/java
-mkdir antlr_generated/python
+mkdir antlr_generated/py
 printf 'Done\n'
 printf 'Generating Java lexer and parser...'
 java -jar /usr/local/lib/antlr-4.5.3-complete.jar vhdl.g4 -o antlr_generated/java
@@ -12,5 +14,5 @@ printf 'Compiling Java lexer and parser...'
 javac -classpath /usr/local/lib/antlr-4.5.3-complete.jar antlr_generated/java/*.java
 printf 'Done\n'
 printf 'Generating Python3 lexer and parser...'
-java -jar /usr/local/lib/antlr-4.5.3-complete.jar -Dlanguage=Python3 vhdl.g4 -o antlr_generated/python
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar -Dlanguage=Python3 vhdl.g4 -o antlr_generated/py
 printf 'Done\n'
