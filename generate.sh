81508a23b45a48307fc9836d9566d09573d8d932
popas90 <bogdititupopa@gmail.com>
2016-09-01 15:01:00 +0300

    generate script working

--- generate.sh
+++ generate.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-rm antlr_generated
+printf 'Creating folder tree...'
+rm -r antlr_generated
 mkdir antlr_generated
 mkdir antlr_generated/java
 mkdir antlr_generated/python
+printf 'Done\n'
+printf 'Generating Java lexer and parser...'
 java -jar /usr/local/lib/antlr-4.5.3-complete.jar vhdl.g4 -o antlr_generated/java
-javac antlr_generated/java/*.java
+printf 'Done\n'
+printf 'Compiling Java lexer and parser...'
+javac -classpath /usr/local/lib/antlr-4.5.3-complete.jar antlr_generated/java/*.java
+printf 'Done\n'
+printf 'Generating Python3 lexer and parser...'
 java -jar /usr/local/lib/antlr-4.5.3-complete.jar -Dlanguage=Python3 vhdl.g4 -o antlr_generated/python
+printf 'Done\n'
