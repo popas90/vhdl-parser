0dd2b9d17241d08e55ff32e504d53e289e5e21ce
popas90 <bogdititupopa@gmail.com>
2016-09-01 14:36:13 +0300

    setup and generate scripts fixes

--- generate.sh
+++ generate.sh
@@ -3,6 +3,6 @@ rm antlr_generated
 mkdir antlr_generated
 mkdir antlr_generated/java
 mkdir antlr_generated/python
-antlr4 vhdl.g4 -o antlr_generated/java
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar vhdl.g4 -o antlr_generated/java
 javac antlr_generated/java/*.java
-antlr4 -Dlanguage=Python3 vhdl.g4 -o antlr_generated/python
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar -Dlanguage=Python3 vhdl.g4 -o antlr_generated/python
