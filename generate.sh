60fc105387051f854f0c22525e59e53c2120bfa2
popas90 <bogdititupopa@gmail.com>
2016-09-01 23:19:50 +0300

    started test infrastructure work

--- generate.sh
+++ generate.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 printf 'Creating folder tree...'
-if [ -d "antlr_generated" ]; then
-  rm -rdf antlr_generated/
+if [ -d "generated" ]; then
+  rm -rdf generated/
 fi
-mkdir antlr_generated
-mkdir antlr_generated/java
-mkdir antlr_generated/py
+mkdir generated
+mkdir generated/java
+mkdir generated/py
 printf 'Done\n'
 printf 'Generating Java lexer and parser...'
-java -jar /usr/local/lib/antlr-4.5.3-complete.jar vhdl.g4 -o antlr_generated/java
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar vhdl.g4 -o generated/java
 printf 'Done\n'
 printf 'Compiling Java lexer and parser...'
-javac -classpath /usr/local/lib/antlr-4.5.3-complete.jar antlr_generated/java/*.java
+javac -classpath /usr/local/lib/antlr-4.5.3-complete.jar generated/java/*.java
 printf 'Done\n'
 printf 'Generating Python3 lexer and parser...'
-java -jar /usr/local/lib/antlr-4.5.3-complete.jar -Dlanguage=Python3 vhdl.g4 -o antlr_generated/py
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar -Dlanguage=Python3 vhdl.g4 -o generated/py
 printf 'Done\n'
---
+++ test/unit_tests.py
@@ -0,0 +1,56 @@
+from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
+from generated.VhdlLexer import VhdlLexer
+from generated.VhdlParser import VhdlParser
+from VhdlListenerForGraph import VhdlListenerForGraph
+from vhdl_model.Association import Association
+import nose
+
+
+def parse_file_setup(file_path):
+    inp = FileStream(file_path)
+    lexer = VhdlLexer(inp)
+    stream = CommonTokenStream(lexer)
+    parser = VhdlParser(stream)
+    listener = VhdlListenerForGraph()
+    tree = parser.design_file()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    return listener
+
+
+def test_two_instances():
+    listener = parse_file_setup('./tests/testfiles/struct_with_components.vhd')
+    port_map0 = [Association("a"), Association("b"), Association("tmp")]
+    port_map1 = [Association("tmp"), Association("c")]
+    nose.tools.eq_(len(listener.instances_list), 2)
+    nose.tools.eq_(listener.instances_list[0].name, 'xor2')
+    nose.tools.eq_(listener.instances_list[0].label, 'u0')
+    nose.tools.eq_(listener.instances_list[0].generic_map, None)
+    nose.tools.eq_(listener.instances_list[0].port_map.elements, port_map0)
+    nose.tools.eq_(listener.instances_list[1].name, 'inv')
+    nose.tools.eq_(listener.instances_list[1].label, 'u1')
+    nose.tools.eq_(listener.instances_list[1].generic_map, None)
+    nose.tools.eq_(listener.instances_list[1].port_map.elements, port_map1)
+
+
+def test_zero_instances():
+    listener = parse_file_setup('./tests/testfiles/d_flop_behav.vhd')
+    nose.tools.eq_(len(listener.instances_list), 0)
+
+
+def test_one_process():
+    listener = parse_file_setup('./tests/testfiles/ac_behave.vhd')
+    sensitivity_list = {'clr', 'clk', 'la', 'ea', 'd'}
+    outputs_list = {'q_data', 'q_alu'}
+    inputs_list = {'d', 'ea'}
+    nose.tools.eq_(len(listener.processes_list), 1)
+    nose.tools.eq_(listener.processes_list[0].label, 'main')
+    nose.tools.eq_(listener.processes_list[0].sensitivity_list,
+                   sensitivity_list)
+    nose.tools.eq_(listener.processes_list[0].outputs, outputs_list)
+    nose.tools.eq_(listener.processes_list[0].inputs, inputs_list)
+
+
+def test_zero_processes():
+    listener = parse_file_setup('./tests/testfiles/struct_with_components.vhd')
+    nose.tools.eq_(len(listener.processes_list), 0)
