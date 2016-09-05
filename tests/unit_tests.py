7f8a0aa386bff04494e80f3175ce34be1889a337
popas90 <bogdititupopa@gmail.com>
2016-09-05 23:14:25 +0300

    use mockito for visitor testing

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,6 +3,7 @@ from generated.VhdlLexer import VhdlLexer
 from generated.VhdlParser import VhdlParser
 from src.ConcreteVhdlVisitor import ConcreteVhdlVisitor
 import nose
+from mockito import mock, when
 
 
 def setup_visitor(input_stream, start_rule):
@@ -50,12 +51,25 @@ def test_entity_declaration_empty():
     visitor = parse_string(entity)
     nose.tools.eq_(visitor.entities[0].name, 'Adder')
 
-
+# TODO keep this out until mocking is in place
+"""
 def test_port_declaration():
-    entity = """entity Adder is
+    entity = ""entity Adder is
              port(rst : in std_logic)
-             end entity Adder;"""
+             end entity Adder;""
     visitor = parse_string(entity)
     nose.tools.eq_(visitor.entities[0].ports[0].name, 'rst')
     nose.tools.eq_(visitor.entities[0].ports[0].direction, 'in')
     nose.tools.eq_(visitor.entities[0].ports[0].type, 'std_logic')
+"""
+
+
+def test_identifier():
+    visitor = ConcreteVhdlVisitor()
+    identifier_node = mock()
+    basic_identifier = mock()
+    extended_identifier = mock()
+    when(basic_identifier).getText().thenReturn('basic')
+    when(extended_identifier).getText().thenReturn('extended')
+    when(identifier_node).BASIC_IDENTIFIER().thenReturn(basic_identifier)
+    nose.tools.eq_(visitor.visitIdentifier(identifier_node), 'basic')
