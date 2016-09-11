5b9220f8d4e024292fb84bb2f3190e852247439c
popas90 <bogdititupopa@gmail.com>
2016-09-11 11:43:18 +0300

    fix test runner script

--- run_tests_full.sh
+++ run_tests_full.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -o errexit
+printf "Running code analysis...\n"
 flake8 --show-source --exclude=generated/,.git/,__pycache__ .
+printf "Code analysis passed.\n"
+printf "\nRunning tests...\n"
 nosetests -v --with-coverage --cover-erase --cover-html --cover-min-percentage=100 --cover-inclusive --cover-package=src
