6bbe3474d2ef0a5084f326a6131eea79b2ebcf68
popas90 <bogdititupopa@gmail.com>
2016-09-12 22:49:35 +0300

    add branch coverage

--- run_tests_full.sh
+++ run_tests_full.sh
@@ -4,4 +4,4 @@ printf "\nRunning code analysis...\n"
 flake8 --show-source --exclude=generated/,.git/,__pycache__ .
 printf "Code analysis passed.\n"
 printf "\nRunning tests...\n"
-nosetests -v --with-coverage --cover-erase --cover-html --cover-min-percentage=100 --cover-inclusive --cover-package=src
+nosetests -v --cover-branches --with-coverage --cover-erase --cover-html --cover-min-percentage=100 --cover-inclusive --cover-package=src
