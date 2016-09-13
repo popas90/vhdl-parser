3e631e3290c7d9f86b1197c2bff3ece050f8d29d
popas90 <bogdititupopa@gmail.com>
2016-09-13 23:04:25 +0300

    format entity.vhd

--- assets/entity.vhd
+++ assets/entity.vhd
@@ -2,10 +2,11 @@ ENTITY ALU IS
 generic(
   Depth : natural := 2;
   Overflow : boolean
-)
-port (A	: in  std_logic_vector(7 downto 0);		--! ALU A input 8-bit from AC
-		B 	: in  std_logic_vector(7 downto 0);		--! ALU B input 8-bit from B-register
-      S 	: out std_logic_vector(Depth-1 downto 0);		--! ALU output 8-bit to W-bus
-		Su	: in  std_logic;								--! Low Add, High Sub
-	   Eu	: in  std_logic);								--! Active low enable ALU (tri-state)
+);
+port (
+  A	 : in  std_logic_vector(7 downto 0);		--! ALU A input 8-bit from AC
+	B	 : in  std_logic_vector(7 downto 0);		--! ALU B input 8-bit from B-register
+  S  : out std_logic_vector(Depth-1 downto 0);		--! ALU output 8-bit to W-bus
+	Su : in  std_logic;								--! Low Add, High Sub
+	Eu : in  std_logic);								--! Active low enable ALU (tri-state)
 END ALU ;
