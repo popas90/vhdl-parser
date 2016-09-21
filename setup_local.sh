705cc2626f62f0949bf33d1c472b4c3caacf0bd7
popas90 <bogdititupopa@gmail.com>
2016-09-21 22:50:49 +0300

    remove setup_local.sh

--- setup_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[ -d "lib" ] || mkdir lib
-chown $USER -R lib/
-cd lib
-wget http://www.antlr.org/download/antlr-4.5.3-complete.jar
-export CLASSPATH=".:$(pwd)/lib/antlr-4.5.3-complete.jar:$CLASSPATH"
